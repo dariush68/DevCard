ec98f65fc2c5bcac4a395639116cb20cb12bdb90
Dariush <Dariush@DESKTOP-I575Q60>
2021-04-10 14:37:28 +0430

    implement contact form validation in client side.

--- CardDev/DevCard_MVC/Models/Contact.cs
+++ CardDev/DevCard_MVC/Models/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,13 @@ namespace DevCard_MVC.Models
 {
     public class Contact
     {
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+        [MinLength(3, ErrorMessage = "حداقل طول کاراکتر 3 است")]
+        [MaxLength(100, ErrorMessage = "حداکثر 100 کاراکتر")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+        [EmailAddress(ErrorMessage = "فرمت ناصحیح ایمیل")]
         public string Email { get; set; }
         public string Message { get; set; }
         public string Service { get; set; }
