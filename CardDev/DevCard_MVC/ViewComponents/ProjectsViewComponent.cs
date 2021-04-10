28199c1a3a15f1b44d3ce5acaccf0522f03f2f34
Dariush <Dariush@DESKTOP-I575Q60>
2021-04-10 12:23:58 +0430

    moved Projects to ViewComponent

---
+++ CardDev/DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        
+        public IViewComponentResult Invoke(string name)
+        {
+            var projects = new List<Project>()
+            {
+                new Project(1, "تاکسی", "توضیحات پروژه تاکسی", "Mobin", "project-1.jpg"),
+                new Project(2, "رستوران هوشمند", "توضیحات پروژه رستوران", "Mobin", "project-2.jpg"),
+                new Project(3, "لیدرمن", "توضیحات پروژه لیدرمن", "Mobin", "project-3.jpg"),
+                new Project(4, "مطالعان", "توضیحات پروژه مطالعان", "Mobin", "project-4.jpg"),
+            };
+
+            return View("_Projects", projects);
+        }
+    }
+}
