81e8be1c3821071f78ae7d1bba4ce34a0aac9cd6
Dariush <Dariush@DESKTOP-I575Q60>
2021-04-10 14:15:34 +0430

    recive form data from html to controler in post method

--- CardDev/DevCard_MVC/Controllers/HomeController.cs
+++ CardDev/DevCard_MVC/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace DevCard_MVC.Controllers
 {
@@ -21,10 +22,26 @@ namespace DevCard_MVC.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Contact()
         {
             return View();
         }
+
+        // [HttpPost]
+        // public JsonResult Contact(IFormCollection form)
+        // {
+        //     var name = form["name"];
+        //     return Json(Ok());
+        // }
+
+        [HttpPost]
+        public JsonResult Contact(Contact form)
+        {
+            Console.WriteLine(form.ToString());
+            return Json(Ok());
+        }
+
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
