919c507dd6d3f9c0dc631a987b86e569f003d1bd
Dariush <Dariush@DESKTOP-I575Q60>
2021-04-11 00:16:58 +0430

    add custom middleware

---
+++ CardDev/DevCard_MVC/CustomLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace DevCard_MVC
+{
+    public class CustomLogger
+    {
+        //-- next middleware --//
+        private RequestDelegate _next;
+
+        public CustomLogger(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            //-- middleware body --//
+
+
+            return _next(context);
+        }
+    }
+
+    public static class CustomLoggerExtension
+    { 
+        public static IApplicationBuilder UseCustomLogger(this IApplicationBuilder applicationBuilder)
+        {
+            return applicationBuilder.UseMiddleware<CustomLogger>();
+        }
+    }
+
+    
+}
--- CardDev/DevCard_MVC/Startup.cs
+++ CardDev/DevCard_MVC/Startup.cs
@@ -29,6 +29,31 @@ namespace DevCard_MVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //-- custom middleware --//
+            app.UseCustomLogger();
+
+
+            //-- UseWhen --//
+            app.UseWhen(context => context.Request.Query.ContainsKey("title"), builder =>
+            {
+                builder.Run(async context =>
+                {
+                    var title = context.Request.Query["title"];
+                    await context.Response.WriteAsync($"title is: {title}");
+                });
+            });
+
+            //-- MapWhen --//
+            app.MapWhen(context => context.Request.Query.ContainsKey("branch"), builder =>
+            {
+                builder.Run(async context =>
+                {
+                    var branch = context.Request.Query["branch"];
+                    await context.Response.WriteAsync($"branch is: {branch}");
+                });
+            });
+
+
             //-- Use --//
             app.Use(async (context, next) =>
             {
@@ -52,7 +77,40 @@ namespace DevCard_MVC
                 await next.Invoke();
             });
             
+            
             //-- Map --//
+            app.Map("/products", appBuilder =>
+            {
+                appBuilder.Map("/Details", builder =>
+                {
+                    builder.Run(async context =>
+                    {
+                        await context.Response.WriteAsync("this is product detail page");
+                    });
+                });
+
+                appBuilder.Use(async (context, next) =>
+                {
+                    var name = context.Request.Query["name"];
+                    if (!string.IsNullOrWhiteSpace(name))
+                    {
+                        context.Items.Add("name", name);
+                    }
+                    await next.Invoke();
+                });
+
+                appBuilder.Run(async context =>
+                {
+                    //context.Items.TryGetValue("name", out var name);
+                    var name = context.Items["name"];
+                    if (!string.IsNullOrWhiteSpace(name.ToString()))
+                    {
+                        name = "Dariush";
+                    }
+                    await context.Response.WriteAsync($"my name is: {name}");
+                });
+            });
+
 
             //-- Run (last middleware, terminator middleware, after that return response) --//
             app.Run(async context =>
